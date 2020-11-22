ca0542d09e09a85891460c36f1b11cdb5968d8dc
KPWysocki <59768608+KPWysocki@users.noreply.github.com>
2020-11-22 10:37:16 +0100

    Add files via upload

---
+++ Zad4-5/ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ile elementów ma być w tablicy?");
+            int n = int.Parse(Console.ReadLine());
+            int[] tab1 = new int[n];
+            int[] tab2 = new int[n];
+            while (n == 0)
+            {
+                Console.WriteLine("Prosze podac liczbe wieksza od 0");
+                n = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i<n; i++)
+            {
+                Console.WriteLine("Podaj {0} element listy", i + 1);
+                tab1[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < n - 1; i++)
+            {
+                if (i != n)
+                {
+                    tab2[i+1] = tab1[i];
+                }
+                tab2[0] = tab1[n-1];
+            }
+            Console.WriteLine("Wartosci tablicy docelowej:");
+            for (int i=0;i<n;i++)
+            {
+
+                Console.Write(tab2[i]+", ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Zad4-7/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] A = { { 1, 2, 3 }, { 4, 5, 6 } };
+            int[,] B = { { 8, 5, 6 }, { 2, 4, 5 } };
+            int[,] C = new int[2, 3];
+            for (int j=0; j<3; j++)
+            {
+                for (int i=0;i<2;i++)
+                {
+                    C[i, j] = A[i, j] + B[i, j];
+                }
+                
+            }
+            Console.WriteLine("Macierz C");
+            for (int j = 0; j < 3; j++)
+            {
+                for (int i = 0; i < 2; i++)
+                {
+                    Console.Write(C[i, j] + " ");
+                }
+                Console.Write("\n");
+            }
+            Console.ReadKey();
+        }
+    }
+}
